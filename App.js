b5eabc3d7133d285ef53eb1af9bae2b93225ce72
Boonyisa Chimaoy <ongsaopym@gmail.com>
2022-08-31 11:41:53 +0700

    8/31/2022

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Button, TextInput } from 'react-native'
+import React from 'react'
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem, DrawerContent } from '@react-navigation/drawer';
+
+function HomeScreen ({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Feed ({ navigation }){
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Feed Screen</Text>
+    </View>
+  )
+}
+
+function Article ({ navigation }){
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Article Screen</Text>
+    </View>
+  )
+}
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props}/>
+      <DrawerItem label='Help' onPress={() => alert('Link to help')} />
+    </DrawerContentScrollView>
+  )
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: '#c6cbef',
+          width: 240,
+        },
+      }}
+      drawerContent={(props) => <CustomDrawerContent{...props} />}
+      >
+        <Drawer.Screen name="Feed" component={Feed} />
+        <Drawer.Screen name="Article" component={Article} />
+        </Drawer.Navigator>
+        );
+      }
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator useLegacyImplementation>
+        <Drawer.Screen name = "Home" component={HomeScreen}/>
+        <Drawer.Screen name = "Notifications" component={Notifications} />
+      </Drawer.Navigator>
+    </NavigationContainer> 
+  )
+  
+}
+
+export default App;
+  
